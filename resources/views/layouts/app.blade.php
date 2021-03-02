1e9236d910437412b9607e854c2747e30e02db96
kevino29 <varax297@gmail.com>
2021-03-02 17:15:05 -0400

    added title icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" href="/assets/images/icons/kevin_icon.ico">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
