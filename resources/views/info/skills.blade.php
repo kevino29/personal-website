0379919dd89cf729a00653b5dfadda6608e825fc
kevino29 <varax297@gmail.com>
2021-03-04 22:41:03 -0400

    updated the skills page

--- resources/views/info/skills.blade.php
+++ resources/views/info/skills.blade.php
@@ -65,27 +65,27 @@
                 </div>
                 <div class="card-body overflow-auto">
                     <ul class="d-flex justify-content-center list-unstyled icon-list mx-auto mt-3">
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <i class="fab fa-python theme-fg lg-font-size"></i>
                             <div>Python <i class="fas fa-check-circle text-primary"></i></div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <i class="fab fa-java theme-fg lg-font-size"></i>
                             <div>Java</div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <div class="img-icon">
                                 <img src="/assets/images/icons/c.png" alt="C Icon">
                             </div>
                             <div>C</div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <div class="img-icon">
                                 <img src="/assets/images/icons/cpp.png" alt="C++ Icon">
                             </div>
                             <div>C++ <i class="fas fa-check-circle text-primary"></i></div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <div class="img-icon">
                                 <img src="/assets/images/icons/cs.png" alt="C# Icon">
                             </div>
@@ -94,19 +94,19 @@
                     </ul>
 
                     <ul class="d-flex justify-content-center list-unstyled icon-list mt-3 mx-auto">
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <i class="fab fa-html5 theme-fg lg-font-size"></i>
                             <div>HTML/HTML5 <i class="fas fa-check-circle text-primary"></i></div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <i class="fab fa-css3 theme-fg lg-font-size"></i>
                             <div>CSS/CSS3 <i class="fas fa-check-circle text-primary"></i></div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <i class="fab fa-js-square theme-fg lg-font-size"></i>
                             <div>JavaScript <i class="fas fa-check-circle text-primary"></i></div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <i class="fab fa-php theme-fg lg-font-size"></i>
                             <div>PHP <i class="fas fa-check-circle text-primary"></i></div>
                         </li>
@@ -126,52 +126,52 @@
 
                 <div class="card-body overflow-auto">
                     <ul class="d-flex justify-content-center list-unstyled icon-list mt-3 mx-auto">
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <i class="fas fa-database theme-fg lg-font-size"></i>
                             <div>MariaDB</div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <i class="fas fa-server theme-fg lg-font-size"></i>
                             <div>SQL Server</div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <i class="fab fa-windows theme-fg lg-font-size"></i>
                             <div>Windows</div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <i class="fab fa-linux theme-fg lg-font-size"></i>
                             <div>Linux</div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <i class="fab fa-android theme-fg lg-font-size"></i>
                             <div>Android</div>
                         </li>
                     </ul>
 
                     <ul class="d-flex justify-content-center list-unstyled icon-list mt-3 mx-auto">
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <i class="fab fa-microsoft theme-fg lg-font-size"></i>
                             <div>MS Office</div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <div class="img-icon">
                                 <img src="/assets/images/icons/jetbrains.png" alt="JetBrains">
                             </div>
                             <div>JetBrains</div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <div class="img-icon">
                                 <img src="/assets/images/icons/vs_code.png" alt="VS Code">
                             </div>
                             <div>VS Code</div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <div class="img-icon">
                                 <img src="/assets/images/icons/visual_studio.png" alt="Visual Studio">
                             </div>
                             <div>Visual Studio</div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <div class="img-icon">
                                 <img src="/assets/images/icons/android_studio.png" alt="Android Studio">
                             </div>
@@ -180,27 +180,27 @@
                     </ul>
 
                     <ul class="d-flex justify-content-center list-unstyled icon-list mt-3 mx-auto">
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <div class="img-icon">
                                 <img src="/assets/images/icons/heroku.png" alt="Heroku">
                             </div>
                             <div>Heroku</div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <i class="fab fa-git theme-fg lg-font-size"></i>
                             <div>Git</div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <i class="fab fa-github theme-fg lg-font-size"></i>
                             <div>GitHub</div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <div class="img-icon">
                                 <img src="/assets/images/icons/google_domains.png" alt="Google Domains">
                             </div>
                             <div>Google Domains</div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <div class="img-icon">
                                 <img src="/assets/images/icons/aws.png" alt="Amazon Web Services">
                             </div>
@@ -220,19 +220,19 @@
                 </div>
                 <div class="card-body overflow-auto">
                     <ul class="d-flex justify-content-center list-unstyled icon-list mt-3 mx-auto">
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <i class="fab fa-bootstrap theme-fg lg-font-size"></i>
                             <div>Bootstrap</div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <i class="fab fa-laravel theme-fg lg-font-size"></i>
                             <div>Laravel</div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <i class="fab fa-node theme-fg lg-font-size"></i>
                             <div>Node.js</div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <div class="img-icon">
                                 <img src="/assets/images/icons/microsoft_dot_net.png" alt=".NET Framework">
                             </div>
@@ -241,13 +241,13 @@
                     </ul>
 
                     <ul class="d-flex justify-content-center list-unstyled icon-list mt-3 mx-auto">
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <div class="img-icon-long">
                                 <img src="/assets/images/icons/phaser.png" alt="Phaser">
                             </div>
                             <div>Phaser</div>
                         </li>
-                        <li class="col text-center">
+                        <li class="col text-center zoom">
                             <div class="img-icon-long">
                                 <img src="/assets/images/icons/express.png" alt="Express.js">
                             </div>
